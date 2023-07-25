4717fd6ac83be017bef3a0107c67596b3a8c4eb7
rahul8899 <rahulkaklotar00@gmail.com>
2023-07-25 19:58:16 +0530

    joi validation for user.schema

--- routes/auth.routes.ts
+++ routes/auth.routes.ts
@@ -1,12 +1,14 @@
 import { Router } from "express";
 import { authController } from "../controller/auth.controller";
+import { bodyValidator } from "../middeleware/validate.schema.middeleware";
+import { usersSchema } from "../schema.validation/users.schema";
 
 export class authRoutes {
     router = Router();
     private ac: authController = new authController();
     constructor() {
         // Route to register 
-        this.router.post('/register', this.ac.registerUser)
+        this.router.post('/register', bodyValidator(usersSchema), this.ac.registerUser)
 
         //Route to login into system
         this.router.post('/login', this.ac.loginUser)
--- routes/user.routes.ts
+++ routes/user.routes.ts
@@ -1,6 +1,8 @@
 import { Router } from "express";
 import { userController } from "../controller/users.controller";
 import { authenticateToken } from "../middeleware/auth.middleware";
+import { bodyValidator } from "../middeleware/validate.schema.middeleware";
+import { usersSchema } from "../schema.validation/users.schema";
 export class userRoutes {
     router = Router();
     private uc: userController = new userController();
@@ -13,7 +15,7 @@ export class userRoutes {
         this.router.get('/getbyid/:userId', this.uc.getUserByID);
 
         // Route to update user details
-        this.router.put('/update/:userId', this.uc.updateUser);
+        this.router.put('/update/:userId', bodyValidator(usersSchema), this.uc.updateUser);
 
         // Route to delete a user
         this.router.delete('/delete/:userId', this.uc.deleteUser);
