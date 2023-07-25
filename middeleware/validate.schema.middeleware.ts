f0dc6eac93732bdfa37029208e80e6f00f1e14e8
rahul8899 <rahulkaklotar00@gmail.com>
2023-07-25 19:57:50 +0530

    validate.schema.middelewere create for joi validation

---
+++ middeleware/validate.schema.middeleware.ts
@@ -0,0 +1,13 @@
+import { Request, Response, NextFunction } from "express";
+
+export const bodyValidator = (schema: any) => {
+    return (req: Request, res: Response, next: NextFunction) => {
+        const result = schema.validate(req.body);
+        if (result.error) {
+            console.log("there is error ", result);
+            return res.json(result.error)
+        } else {
+            next();
+        }
+    }
+}
