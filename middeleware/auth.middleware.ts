36aa495690419b713e0a07a0def54ca85846b118
rahul8899 <rahulkaklotar00@gmail.com>
2023-07-27 11:14:15 +0530

    auth middeleware updated

--- middeleware/auth.middleware.ts
+++ middeleware/auth.middleware.ts
@@ -1,52 +1,20 @@
-import jwt, { JsonWebTokenError } from 'jsonwebtoken';
-import { Users } from '../models/users'; // Replace this with your Users model
-import { NextFunction } from 'express';
-import { Request, Response } from "express";
-import { decode } from 'jsonwebtoken';
+import { Request, Response, NextFunction } from "express";
+import jwt from "jsonwebtoken";
+import { JWT_SECRET } from "../controller/auth.controller"; // Import JWT_SECRET from the auth.controller
 
-const tokenSecret: any = process.env.JWT_SECREAT_KEY; // Replace with your JWT secret
+export const authMiddleware = (req: Request, res: Response, next: NextFunction) => {
+    const token = req.header("Authorization")?.replace("Bearer ", "");
 
-
-
-
-// Middleware to authenticate the JWT token
-export const authenticateToken = async (req: Request, res: Response, next: NextFunction) => {
-    try {
-        const token: string | null =
-            "token" in req.headers ? (req.headers.token as string) : null;
-        let jwtPayload;
-        const secret: string = process.env.JWT_SECRET || "";
-
-        if (token) {
-            //Try to validate the token and get data
-            try {
-                jwtPayload = <any>jwt.verify(token, secret);
-                res.locals.jwtPayload = jwtPayload;
-                res.locals.user = res.locals.jwtPayload.data;
-            } catch (error) {
-                //If token is not valid, respond with 401 (unauthorized)
-                res.status(401).json({ message: "Invalid auth token provided or user unauthorized!", error });
-                return;
-            }
-
-            //The token is valid for a time you entered inside .env file
-            //We want to send a new token on every request
-            const { userId, email } = jwtPayload;
-            const newToken = jwt.sign({ userId, email }, secret, {
-                expiresIn: process.env.JWT_MAX_AGE,
-                algorithm: "HS256",
-            });
-            res.setHeader("token", newToken);
-            //Call the next middleware or controller
-            next();
-        }
-        else {
-            res.json({
-                message: "Something went wrong! Please try again later...",
-            });
-        }
+    if (!token) {
+        return res.status(401).json({ message: "Token missing" });
     }
-    catch (err) {
-        return res.status(403).json({ error: 'Invalid token. ' });
+
+    try {
+        const decodedToken = jwt.verify(token, JWT_SECRET) as { userId: number };
+        req.userId = decodedToken.userId;
+        next();
+    } catch (err) {
+        console.error("Invalid token:", err);
+        return res.status(401).json({ message: "Invalid token" });
     }
 };
