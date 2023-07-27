17d1daf88bcac947f562e6df408ccce80c004462
rahul8899 <rahulkaklotar00@gmail.com>
2023-07-27 11:29:43 +0530

    auth middeleware implement on appointment

--- controller/appointments.controller.ts
+++ controller/appointments.controller.ts
@@ -94,7 +94,7 @@ export class appointmentController {
                     message: "Appointment is deleted"
                 })
             } else {
-                return res.status(404).json({ message: "Appointment is not deleted" })
+                return res.status(404).json({ message: "Appointment is not founded." })
             }
         } catch (error) {
             console.error("Error in deleting appoinment", error);
--- routes/appointment.routes.ts
+++ routes/appointment.routes.ts
@@ -1,6 +1,7 @@
 import { Router } from "express";
 import { appointmentController } from "../controller/appointments.controller";
 import { bodyValidator } from "../middeleware/validate.schema.middeleware";
+import { authMiddleware } from "../middeleware/auth.middleware";
 import { appointmentSchema } from "../schema.validation/appointment.schema";
 export class appointmentRoutes {
     router = Router();
@@ -8,18 +9,18 @@ export class appointmentRoutes {
     constructor() {
 
         // Route to fetch all appontments
-        this.router.get('/getall', this.apc.getAllAppointment);
+        this.router.get('/getall', authMiddleware, this.apc.getAllAppointment);
 
         // Route to get a specific appontment by ID
-        this.router.get('/getbyid/:appointmentId', this.apc.getAppointmentById);
+        this.router.get('/getbyid/:appointmentId', authMiddleware, this.apc.getAppointmentById);
 
         // Route to create appontment 
-        this.router.post('/create', bodyValidator(appointmentSchema), this.apc.createAppointment)
+        this.router.post('/create', authMiddleware, bodyValidator(appointmentSchema), this.apc.createAppointment)
 
         // Route to update appontment details
-        this.router.put('/update/:appointmentId', bodyValidator(appointmentSchema), this.apc.updateAppointment);
+        this.router.put('/update/:appointmentId', authMiddleware, bodyValidator(appointmentSchema), this.apc.updateAppointment);
 
         // Route to delete a test
-        this.router.delete('/delete/:appointmentId', this.apc.deleteAppointment);
+        this.router.delete('/delete/:appointmentId', authMiddleware, this.apc.deleteAppointment);
     }
 }
