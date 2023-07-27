5790cbee07694ac7c3798ec4597015c51b16d8f1
rahul8899 <rahulkaklotar00@gmail.com>
2023-07-27 11:23:35 +0530

    auth middeleware implement on medicaltest

--- routes/medicalTest.routes.ts
+++ routes/medicalTest.routes.ts
@@ -1,6 +1,7 @@
 import { Router } from "express";
 import { medicalTestsController } from "../controller/medicaltests.controller";
 import { bodyValidator } from "../middeleware/validate.schema.middeleware";
+import { authMiddleware } from "../middeleware/auth.middleware";
 import { medicalTestSchema } from "../schema.validation/medicaltest.schema";
 
 export class medialTestRoutes {
@@ -9,18 +10,18 @@ export class medialTestRoutes {
     constructor() {
 
         // Route to fetch all test
-        this.router.get('/getall', this.mc.getTest);
+        this.router.get('/getall', authMiddleware, this.mc.getTest);
 
         // Route to get a specific test by ID
-        this.router.get('/getbyid/:testId', this.mc.getTestByID);
+        this.router.get('/getbyid/:testId', authMiddleware, this.mc.getTestByID);
 
         // Route to create test 
-        this.router.post('/create', bodyValidator(medicalTestSchema), this.mc.createTest)
+        this.router.post('/create', authMiddleware, bodyValidator(medicalTestSchema), this.mc.createTest)
 
         // Route to update test details
-        this.router.put('/update/:testId', bodyValidator(medicalTestSchema), this.mc.updateTest);
+        this.router.put('/update/:testId', authMiddleware, bodyValidator(medicalTestSchema), this.mc.updateTest);
 
         // Route to delete a test
-        this.router.delete('/delete/:testId', this.mc.deleteTest);
+        this.router.delete('/delete/:testId', authMiddleware, this.mc.deleteTest);
     }
 }
